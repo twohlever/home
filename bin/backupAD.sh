f53071ff94800805a860fec238aabca22d8ca52f
Theresa <twohlever@users.noreply.github.com>
2022-01-18 13:35:09 -0500

    Fix File path escape issue

--- bin/backupAD.sh
+++ bin/backupAD.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
 ## Backup paths
-LOCAL_AD="${HOME}Amazon\ Drive"
-EXT_HD_AD="/Volumes/Seagate/Amazon\ Drive"
-NAS_AD="/Volumes/Wohlever_Media/Amazon\ Drive"
+LOCAL_AD="${HOME}/Amazon Drive"
+EXT_HD_AD="/Volumes/Seagate/Amazon Drive"
+NAS_AD="/Volumes/Wohlever_Media/Amazon Drive"
 
 
 ## Executables
 BIN="${HOME}/bin"
 BREW_BIN="/opt/homebrew/bin"
 
-SYNC="${BREW_BIN}/rclone sync -P " ‪## https://rclone.org/commands/rclone_sync/
+SYNC_CMD="${BREW_BIN}/rclone sync -P"
+## https://rclone.org/commands/rclone_sync/
 
 
 ## Organize Photos
-# ${BIN}/organize-pictures.sh
+echo "Organize Photos"
+${BIN}/organize-pictures.sh
 
 
 ## Back it up
@@ -22,35 +24,35 @@ SYNC="${BREW_BIN}/rclone sync -P " ‪## https://rclone.org/commands/rclone_sync
 SOURCE="${LOCAL_AD}/Documents/"
 DEST="${EXT_HD_AD}/Documents/"
 echo ""; echo ""; echo "";
-echo "${SYNC} ${SOURCE} ${DEST}"
-# ${SYNC} ${SOURCE} ${DEST}
+echo "${SYNC_CMD} ${SOURCE} ${DEST}"
+${SYNC_CMD} "${SOURCE}" "${DEST}"
 
 
 
 SOURCE="${LOCAL_AD}/Audio/"
 DEST="${EXT_HD_AD}/Audio/"
 echo ""; echo ""; echo "";
-echo "${SYNC} ${SOURCE} ${DEST}"
-# ${SYNC} ${SOURCE} ${DEST}
+echo "${SYNC_CMD} ${SOURCE} ${DEST}"
+${SYNC_CMD} "${SOURCE}" "${DEST}"
 
 
 SOURCE="${LOCAL_AD}/Books/"
 DEST="${EXT_HD_AD}/Books/"
 echo ""; echo ""; echo "";
-echo "${SYNC} ${SOURCE} ${DEST}"
-# ${SYNC} ${SOURCE} ${DEST}
+echo "${SYNC_CMD} ${SOURCE} ${DEST}"
+${SYNC_CMD} "${SOURCE}" "${DEST}"
 
 
 SOURCE="${LOCAL_AD}/Pictures/2022/"
 DEST="${EXT_HD_AD}/Pictures/2022/"
 echo ""; echo ""; echo "";
-echo "${SYNC} ${SOURCE} ${DEST}"
-# ${SYNC} ${SOURCE} ${DEST}
+echo "${SYNC_CMD} ${SOURCE} ${DEST}"
+${SYNC_CMD} "${SOURCE}" "${DEST}"
 
 
 # External hard drive to Network Storage
 SOURCE="${EXT_HD_AD}/"
 DEST="${NAS_AD}/"
 echo ""; echo ""; echo "";
-echo "${SYNC} ${SOURCE} ${DEST}"
-# ${SYNC} ${SOURCE} ${DEST}
+# echo "${SYNC_CMD} ${SOURCE} ${DEST}"
+# ${SYNC_CMD} "${SOURCE}" "${DEST}"
